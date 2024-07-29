ad7cd8c4dd101d9b7bc2b7460fdbca1f57b4e12c
Ritajaritz <137432446+Ritajaritz@users.noreply.github.com>
2024-07-29 16:05:40 +0530

    Time: 36 ms (75.82%) | Memory: 17.4 MB (22.60%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -15,10 +15,20 @@ public:
         //creating dummy nodes;
         ListNode*a = headA;
         ListNode*b = headB;
-        while(a!=b){
-           a=a==NULL? headB : a->next;
-           b=b==NULL? headA : b->next;
+        while(a != b) {
+            if(a == NULL) {
+                a = headB;  // If pointer a reaches the end of list A, move it to the head of list B
+            } else {
+                a = a->next;  // Otherwise, move it to the next node
+            }
+
+            if(b == NULL) {
+                b = headA;  // If pointer b reaches the end of list B, move it to the head of list A
+            } else {
+                b = b->next;  // Otherwise, move it to the next node
+            }
         }
+
         return a;
         
     }
