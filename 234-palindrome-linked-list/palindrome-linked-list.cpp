50601b228cc2c39ad48288ff2d6839b70bcc3ccb
Ritajaritz <137432446+Ritajaritz@users.noreply.github.com>
2024-07-29 17:41:11 +0530

    Time: 143 ms (93.43%) | Memory: 116.9 MB (70.75%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return true; // An empty list or a single node list is a palindrome
+        }
+
+        // Find the middle of the list using slow and fast pointers
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // Corrected loop to find the middle of the list
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        // Reverse the second half of the list
+        slow = reverseList(slow);
+        ListNode* secondHalf = slow;
+
+        // Compare the first half and the reversed second half
+        ListNode* firstHalf = head;
+        while (secondHalf != NULL) {
+            if (firstHalf->val != secondHalf->val) {
+                return false;
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+        return true;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        while (head != NULL) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev; // prev will be the new head of the reversed list
+    }
+};
